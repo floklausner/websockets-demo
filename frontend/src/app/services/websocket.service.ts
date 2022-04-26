f217d6025a7fbb0962f64000bceda313c3abc43e
floklausner <klausnerflo@gmail.com>
2022-04-26 09:53:58 +0200

    websocket-util & websocket.service finished

---
+++ frontend/src/app/services/websocket.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {WebSocketUtil} from "../utils/websocket-util";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsocketService {
+
+  private socket: WebSocketUtil<string> | null
+
+  constructor() {
+    this.socket = null
+  }
+
+  connect(): Observable<string> {
+    if (!this.socket) {
+      this.socket = new WebSocketUtil()
+      this.socket.connect()
+
+      this.socket.errorMessages.subscribe(value => {
+        console.log(value);
+      })
+    }
+    return this.socket.messages
+  }
+
+  sendMessage(message: string): void {
+    this.socket?.sendMessage(message)
+  }
+}
---
+++ frontend/src/app/utils/websocket-util.ts
@@ -0,0 +1,36 @@
+import {Subject} from "rxjs";
+import {webSocket, WebSocketSubject} from "rxjs/webSocket";
+
+export class WebSocketUtil<T> {
+  public messages: Subject<T>
+  public errorMessages: Subject<any>
+  public sessionId: number
+  public socket: WebSocketSubject<T> | null
+
+  constructor() {
+    this.messages = new Subject<T>();
+    this.errorMessages = new Subject();
+    this.sessionId = Date.now();
+    this.socket = null;
+  }
+
+  public connect(): void {
+    this.socket = webSocket<T>({
+      url: "ws://localhost:8080/start-websocket/" + this.sessionId,
+      deserializer: msg => msg.data
+    })
+
+    this.socket.subscribe({
+      next: msg => this.messages.next(msg),
+      error: err => this.errorMessages.next(err)
+    })
+  }
+
+  public sendMessage(message: T): void {
+    if (this.socket) {
+      this.socket.next(message);
+    }
+  }
+}
+
+
